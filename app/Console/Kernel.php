50e31545354c8f933369a844223e8f92a16b9b0b
Fuad Rivai <fuad.strummer@gmail.com>
2026-06-02 11:07:41 +0700

    cleaning

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')->minute();
+        $schedule->command('inspire')->everyMinute();
         // $schedule->command('attendance:generate-daily')->dailyAt('01:00');
     }
 
